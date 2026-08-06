789226e548f901e9ff9155747792b2e8c49de235
krishna <pathakrishna2704@gmail.com>
2026-08-06 09:15:39 +0530

    fix: adjust text sizes in Message component for improved readability

--- components/Message.jsx
+++ components/Message.jsx
@@ -14,7 +14,7 @@ const copyMessage=()=>{
     toast.success("Message copied  to clipboard")
 }
   return (
-    <div className='flex flex-col items-center w-full max-w-3xl text-sm '>
+    <div className='flex flex-col items-center w-full max-w-3xl text-[15px]'>
         <div className={`flex flex-col w-full mb-8 ${role==='user' && 'items-end'}`}>
             <div className={`group relative flex max-w-2xl py-3 rounded-xl ${role==='user'?'bg-[#414158] px-5':'gap-3' }`}>
                 <div className={`opacity-0 group-hover:opacity-100 absolute ${role==='user'?'-left-16 top-2.5':'left-9 -bottom-6' } transition-all`}>
@@ -39,7 +39,7 @@ const copyMessage=()=>{
                 </div>
                 {
                     role=='user'?(
-                        <span className='text-white/90'>{content}</span>
+                        <span className='text-white/90 text-[17px]'>{content}</span>
                     ):(
                         <>
                         <Image src={assets.logo_icon} alt='' className='h-9 w-9 p-1
